3814e9cb0f0b3cc9ab86b73434f2f84f1ef0028d
alvaro7913 <alvarocesped@gmail.com>
2022-04-06 19:43:18 -0400

    segundo commit

--- request.rb
+++ request.rb
@@ -1,5 +1,6 @@
 require "uri"
 require "net/http"
+require "json"
 
 url = URI("https://api.nasa.gov/planetary/apod?api_key=ggRQ9HgxkoT1wuW4vr9JDDBSunVQGIUzOXF34Xhg")
 
@@ -9,4 +10,7 @@ https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 
 response = https.request(request)
-puts response.read_body
+results = JSON.parse(response.read_body)
+
+puts results["date"]
+
